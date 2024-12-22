04cfa37df6c8c035e479c49dc81dd076b5e8b9a6
root <alexanderbigman29@gmail.com>
2024-12-22 20:51:28 +0000

    Escribe un programa en C que imprima exactamente with proper grammar, but the outcome is a piece of art,, seguido de una nueva línea

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a string to the standard output
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
