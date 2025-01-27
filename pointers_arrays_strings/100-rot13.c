5404a80c46214267b1eca5bcb5aa02cb5c76b665
maximiliam <alexanderbigman29@gmail.com>
2025-01-27 03:46:58 +0000

    Prototype

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -12,10 +12,18 @@ char *rot13(char *s)
 
     for (i = 0; s[i] != '\0'; i++)
     {
-        if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
-            s[i] += 13;
-        else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
-            s[i] -= 13;
+        if (((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M')) ||
+            ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))) 
+        {
+            if (s[i] >= 'a' && s[i] <= 'm')
+                s[i] += 13;
+            else if (s[i] >= 'A' && s[i] <= 'M')
+                s[i] += 13;
+            else if (s[i] >= 'n' && s[i] <= 'z')
+                s[i] -= 13;
+            else if (s[i] >= 'N' && s[i] <= 'Z')
+                s[i] -= 13;
+        }
     }
 
     return (s);
