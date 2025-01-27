4aac27c6c5326bb9578b65462bdc5f43e2d45fb9
maximiliam <alexanderbigman29@gmail.com>
2025-01-27 03:25:27 +0000

    Prototype

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using rot13.
+ * @s: The string to encode.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *s)
+{
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+            s[i] += 13;
+        else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+            s[i] -= 13;
+    }
+
+    return (s);
+}
