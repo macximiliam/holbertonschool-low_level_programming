f6c96f853cb2501eb4e0ebd8c606564141540625
maximiliam <alexanderbigman29@gmail.com>
2025-02-11 02:46:23 +0000

    Pointers

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -8,19 +8,20 @@
  * @n: Número de bytes a rellenar.
  *
  * Descripción:
- * Esta función itera sobre un bloque de memoria de 'n' bytes, 
- * comenzando en la dirección apuntada por 's', y establece cada byte 
+ * Esta función itera sobre un bloque de memoria de 'n' bytes,
+ * comenzando en la dirección apuntada por 's', y establece cada byte
  * a el valor proporcionado por 'b'.
  *
  * Retorno:
  * Retorna un puntero a la misma área de memoria 's', ahora modificada.
  */
-char *_memset(char *s, char b, unsigned int n) {
-  unsigned int i;
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
 
-  for (i = 0; i < n; i++) {
-    s[i] = b;
-  }
-
-  return s;
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
 }
