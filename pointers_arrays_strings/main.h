fb624638f507491bb46c43a4f8a0795f7bc3501f
maximiliam <alexanderbigman29@gmail.com>
2025-02-13 01:57:01 +0000

    Pointers

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,4 +22,5 @@ int _strcmp(char *s1, char *s2);
  char *rot13(char *);
  int _putchar(char c);
  char *_memset(char *s, char b, unsigned int n);
+ char *_memcpy(char *dest, char *src, unsigned int n);
 #endif/** MAIN_H*/
