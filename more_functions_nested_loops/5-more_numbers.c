70180559bef35e513f5f1b15414ef035d39f8df9
macximiliam <alexanderbigman29@gmail.com>
2025-04-21 02:48:45 +0000

    a function that prints 10 times the numbers, from 0 to 14, followed by a new line

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * 
+ *
  * Return : void
  */
 void more_numbers(void)
@@ -20,7 +20,3 @@ void more_numbers(void)
 		_putchar('\n');
 	}
 }
-
-
-
-    
