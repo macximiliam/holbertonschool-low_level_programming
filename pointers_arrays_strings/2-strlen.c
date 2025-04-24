ab3287be43705255d35647ad56b8b9e5bab56296
macximiliam <alexanderbigman29@gmail.com>
2025-04-24 05:53:09 +0000

    a function that swaps the values of two integers.

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _strlen - Returns the length of a string
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
 int _strlen(char *s)
 { 
     int a = 0;
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef	MAIN_H
 #define MAIN_H
 /*Aqui va el contenido de tu archivo amin.h */
-int _strlen(char *);
+void swap_int(int *a, int *b);
 void reset_to_98(int *);
+int _strlen(char *);
 #endif/*MAIN.H*/
