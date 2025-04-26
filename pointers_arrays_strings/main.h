246ae1d24216b76ef60a2fa174a209337805ca5c
macximiliam <alexanderbigman29@gmail.com>
2025-04-26 00:52:30 +0000

    Add a function that prints n elements of an array of integers, followed by a new line.

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,12 +1,13 @@
 #ifndef	MAIN_H
 #define MAIN_H
 /*Aqui va el contenido de tu archivo amin.h */
-void swap_int(int *a, int *b);
+void swap_int(int *, int *);
 void reset_to_98(int *);
 int _strlen(char *);
-void _puts(char *str);
+void _puts(char *);
 int _putchar(char);
 void print_rev(char *);
-void rev_string(char *s);
+void rev_string(char *);
 void puts2(char *);
+void puts_half(char *);
 #endif/*MAIN.H*/
