695920705bf34049b1944db974ab9ff639089a46
macximiliam <alexanderbigman29@gmail.com>
2025-04-28 04:08:24 +0000

    Add  a function that prints n elements of an array of integers, followed by a new line

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef	MAIN_H
 #define MAIN_H
-/*Aqui va el contenido de tu archivo amin.h */
+
 void swap_int(int *, int *);
 void reset_to_98(int *);
 int _strlen(char *);
@@ -10,4 +10,5 @@ void print_rev(char *);
 void rev_string(char *);
 void puts2(char *);
 void puts_half(char *);
+void print_array(int *, int n);
 #endif/*MAIN.H*/
