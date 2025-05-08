f2d2b7498aa526ecdf27af770013dbd35eefdcc2
macximiliam <alexanderbigman29@gmail.com>
2025-05-08 01:14:20 +0000

    Add a Funtion that gets the length of a prefix substring.

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,29 @@
 #include "main.h"
-#include <string.h>
+
+/**
+ * _strchr - Locates the first occurrence of a character in a string.
+ * @s: Pointer to the string to be searched.
+ * @c: The character to locate.
+ *
+ * Return: A pointer to the first occurrence of
+ * the character c in the string s,
+ * or NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+
+	if (s[i] == c)
+		return (&s[i]);
+
+	return ('\0');
+}
 
 /**
  * _strspn - Funtion that gets the length of a prefix substring.
@@ -10,12 +34,11 @@
  * Return: The number of bytes in the initial segment of s
  * which consist only of bytes from accept
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0;
 
-	for (; s[i] && strchr(accept, s[i]); i++)
+	for (; s[i] && _strchr(accept, s[i]); i++)
 	;
 
 	return (i);
