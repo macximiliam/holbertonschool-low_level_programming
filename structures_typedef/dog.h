71056a0b9bb7c9f20273c9e415d3bd23a5b1afd1
macximiliam <alexanderbigman29@gmail.com>
2025-05-30 00:37:24 +0000

    Add Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+/**
+* struct dog - Define a new type struct dog
+* @name: char * name of the dog
+* @age: float age of the dog
+* @owner: char * owner of the dog
+*
+* Description: Longer description
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
