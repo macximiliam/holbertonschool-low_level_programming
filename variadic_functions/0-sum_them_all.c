6049746d152164f0b1ac218841928e7e7065de0c
macximiliam <alexanderbigman29@gmail.com>
2025-06-14 22:21:41 +0000

    Add variadic function and macros

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -16,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	if (n == 0)
-		return 0;
+		return (0);
 
 	va_start(args, n);
 
@@ -26,5 +26,5 @@ int sum_them_all(const unsigned int n, ...)
 	}
 
 	va_end(args);
-	return sum;
+	return (sum);
 }
--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,41 +3,52 @@
 #include <stdarg.h>
 
 
+/**
+ * print_all - Prints arguments based on a format string.
+ * @format: A string of format specifiers:
+ *          'c' for char, 'i' for int, 'f' for float, 's' for string.
+ * @...:    Variable number of arguments to print.
+ *
+ * Description:
+ * Prints each argument according to its type, separated by ", ".
+ * If a string is NULL, prints "(nil)" instead.
+ * Ends the output with a newline.
+ */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0;
-    char *str;
-    char *separator = "";
+	va_list args;
+	int i = 0;
+	char *str;
+	char *separator = "";
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while(format && format[i])
-    {
-        switch(format[i])
-        {
-            case 'c':
-                printf("%s%c", separator, va_arg(args, int));
-                break;
-            case 'i':
-                printf("%s%d", separator, va_arg(args, int));
-                break;
-            case 'f':
-                printf("%s%f", separator, va_arg(args, double));
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(nil)";
-                printf("%s%s", separator, str);
-                break;
-            default:
-                i++;
-                continue;
-        }
-        separator = ", ";
-        i++;
-    }
-    va_end (args);
-    printf("\n");
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
