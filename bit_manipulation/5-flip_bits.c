19be29fbf2841890b531d07c9f7304fa951ca11f
macximiliam <alexanderbigman29@gmail.com>
2025-07-10 04:53:22 +0000

    Add function that sets the value of a bit to 1 at a given index

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * 
+ * flip_bits - Counts the number of bits to change to convert
+ *             one number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits that need to be flipped.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor_result = n ^ m;
-	unsigned int count = n;
+	unsigned int count = 0;
 
 	while (xor_result)
 	{
