d78f5f14369c8180ab98444ba694a43519ea53c3
macximiliam <alexanderbigman29@gmail.com>
2025-07-14 03:44:46 +0000

    Add Write a program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,164 +1,105 @@
-#include <stdio.h>
-#include "main.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+# include "main.h"
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 #define BUFFER_SIZE 1024
 
-
 /**
- * check_args - Checks the number of command-line arguments.
- * @argc: The argument count.
- *
- * Description: If argc is not 3, prints a usage message to stderr
- * and exits with code 97.
+ * print_error - Prints an error message to stderr and exits
+ * @code: Exit code
+ * @msg: Error message
+ * @file: File name or descriptor value to include
  */
-void check_args(int argc)
+void print_error(int code, const char *msg, const char *file)
 {
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+	if (file)
+		dprintf(STDERR_FILENO, "%s%s\n", msg, file);
+	else
+		dprintf(STDERR_FILENO, "%s\n", msg);
+	exit(code);
 }
 
 /**
- * handle_error_and_exit - Prints an error message to stderr and exits.
- * @message: The format string for the error message.
- * @filename: The filename to be included in the error message.
- * @exit_code: The exit code for the program.
- *
- * Description: This function centralizes error reporting and program exit.
+ * transfer_data - Reads from fd_from and writes to fd_to in chunks
+ * @fd_from: Source file descriptor
+ * @fd_to: Destination file descriptor
+ * @file_from: Source file name for errors
+ * @file_to: Destination file name for errors
  */
-void handle_error_and_exit(const char *message,
-						   const char *filename, int exit_code)
+void transfer_data(int fd_from, int fd_to, const char *file_from, const char *file_to)
 {
-	dprintf(STDERR_FILENO, message, filename);
-	exit(exit_code);
-}
-
-/**
- * open_from_file - Opens the source file for reading.
- * @filename: The name of the file to open.
- *
- * Return: The file descriptor of the opened file.
- * Description: If the file cannot be opened for reading, prints an error
- * message to stderr and exits with code 98.
- */
-int open_from_file(const char *filename)
-{
-	int fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		handle_error_and_exit("Error: Can't read from file %s\n",
-							  filename, 98);
-	}
-	return (fd);
-}
-
-/**
- * open_to_file - Opens or creates the destination file for writing.
- * @filename: The name of the file to open/create.
- * @fd_from: The file descriptor of the source file (to close on error).
- *
- * Return: The file descriptor of the opened/created file.
- * Description: If the file cannot be created or written to, prints an error
- * message to stderr and exits with code 99. Closes fd_from
- * if an error occurs here.
- */
-int open_to_file(const char *filename, int fd_from)
-{
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC,
-				  S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd == -1)
-	{
-		close_file_descriptor(fd_from, 100); /* Try to close fd_from */
-		handle_error_and_exit("Error: Can't write to %s\n",
-							  filename, 99);
-	}
-	return (fd);
-}
-
-/**
- * copy_file_content - Copies data from source to destination file.
- * @fd_from: File descriptor of the source file.
- * @fd_to: File descriptor of the destination file.
- * @file_from_name: Name of the source file (for error messages).
- * @file_to_name: Name of the destination file (for error messages).
- *
- * Description: Reads 1024 bytes at a time from fd_from and writes them
- * to fd_to. Handles read/write errors and exits accordingly.
- */
-void copy_file_content(int fd_from, int fd_to,
-					   const char *file_from_name, const char *file_to_name)
-{
-	ssize_t bytes_read, bytes_written;
+	int r_bytes, w_bytes;
 	char buffer[BUFFER_SIZE];
 
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+while ((r_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			close_file_descriptor(fd_from, 100);
-			close_file_descriptor(fd_to, 100);
-			handle_error_and_exit("Error: Can't write to %s\n",
-								  file_to_name, 99);
-		}
+		w_bytes = write(fd_to, buffer, r_bytes);
+		if (w_bytes != r_bytes)
+			print_error(99, "Error: Can't write to ", file_to);
 	}
-
-	if (bytes_read == -1)
-	{
-		close_file_descriptor(fd_from, 100);
-		close_file_descriptor(fd_to, 100);
-		handle_error_and_exit("Error: Can't read from file %s\n",
-							  file_from_name, 98);
-	}
+	if (r_bytes == -1)
+		print_error(98, "Error: Can't read from file ", file_from);
 }
 
 /**
- * close_file_descriptor - Closes a file descriptor.
- * @fd: The file descriptor to close.
- * @exit_code: The exit code to use if closing fails.
- *
- * Description: If closing the file descriptor fails, prints an error
- * message to stderr and exits with the given exit_code.
+ * copy_file - Copies the content of a file to another
+ * @file_from: Source file name
+ * @file_to: Destination file name
  */
-void close_file_descriptor(int fd, int exit_code)
+void copy_file(const char *file_from, const char *file_to)
 {
-	if (close(fd) == -1)
+	int fd_from, fd_to, first_read, first_write;
+	char buffer[BUFFER_SIZE];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		print_error(98, "Error: Can't read from file ", file_from);
+
+	first_read = read(fd_from, buffer, BUFFER_SIZE);
+	if (first_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(exit_code);
+		close(fd_from);
+		print_error(98, "Error: Can't read from file ", file_from);
 	}
+
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		print_error(99, "Error: Can't write to ", file_to);
 }
 
+	first_write = write(fd_to, buffer, first_read);
+	if (first_write != first_read)
+	{
+		close(fd_from);
+		close(fd_to);
+		print_error(99, "Error: Can't write to ", file_to);
+	}
+
+	transfer_data(fd_from, fd_to, file_from, file_to);
+
+	if (close(fd_from) == -1)
+		print_error(100, "Error: Can't close fd ", file_from);
+	if (close(fd_to) == -1)
+		print_error(100, "Error: Can't close fd ", file_to);
+	}
+
 /**
- * main - Copies the content of a file to another file.
- * @argc: Number of arguments passed to the program.
- * @argv: Array of strings with the arguments.
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Return: 0 on success.
- * 97 if the number of arguments is incorrect.
- * 98 if file_from does not exist or cannot be read.
- * 99 if file_to cannot be created or written to.
- * 100 if a file descriptor cannot be closed.
+ * Return: 0 on success, exits with code on failure
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
+	if (argc != 3)
+		print_error(97, "Usage: cp file_from file_to", NULL);
 
-	check_args(argc);
+	copy_file(argv[1], argv[2]);
 
-	fd_from = open_from_file(argv[1]);
-	fd_to = open_to_file(argv[2], fd_from);
-
-	copy_file_content(fd_from, fd_to, argv[1], argv[2]);
-
-	close_file_descriptor(fd_from, 100);
-	close_file_descriptor(fd_to, 100);
-
-	return (0);
+	return(0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -5,12 +5,11 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void check_args(int argc);
-void handle_error_and_exit(const char *message, const char *filename, int exit_code);
-int open_from_file(const char *filename);
-int open_to_file(const char *filename, int fd_from);
-void copy_file_content(int fd_from, int fd_to, const char *file_from_name, const char *file_to_name);
-void close_file_descriptor(int fd, int exit_code);
+int main(int argc, char *argv[]);
+void copy_file(const char *file_from, const char *file_to);
+void transfer_data(int fd_from, int fd_to, const char *file_from, const char *file_to);
+void print_error(int code, const char *msg, const char *file);
+
 
 
 #endif /* MAIN_H_ */
